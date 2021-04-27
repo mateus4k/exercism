50cf5284ba3afb85b8f38a445bcd1f9849bf4154
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-26 21:20:32 -0300

    go: difference_of_squares solution

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,14 @@
+package diffsquares
+
+func SquareOfSum(n int) int {
+	ret := (n * (n + 1)) / 2
+	return ret * ret
+}
+
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
