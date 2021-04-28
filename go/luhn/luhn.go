31cf26b1a34bf5dbf5a33ae2d7b2357fce53669d
Mateus Sampaio <mateus4k@protonmail.ch>
2021-04-27 23:37:35 -0300

    go: luhn solution

---
+++ go/luhn/luhn.go
@@ -0,0 +1,40 @@
+package luhn
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func Valid(s string) bool {
+	var sum int
+	var tag bool = false
+
+	s = strings.ReplaceAll(s, " ", "")
+
+	if len(s) <= 1 {
+		return false
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		r := rune(s[i])
+
+		if !unicode.IsDigit(r) {
+			return false
+		}
+
+		n, _ := strconv.Atoi(string(r))
+
+		if tag {
+			n *= 2
+			if n > 9 {
+				sum -= 9
+			}
+		}
+
+		sum += n
+		tag = !tag
+	}
+
+	return sum%10 == 0
+}
